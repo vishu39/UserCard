1009b6d624cee96a82cb46270c6fd971f2430864
Vishu Sharma <61228845+vishu39@users.noreply.github.com>
2021-11-27 19:23:47 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,135 +1,33 @@
 //---------------------------fetching the user data--------------------------------------------------------
 let fetchUser=async function(){
     const response=await fetch('https://jsonplaceholder.typicode.com/users');
-    var users=await response.json();
-    users.forEach((user) => user);
+    let users=await response.json();
         return users;
 }
 //----------------------------------------------------------------------------------------------------------
 //-------------------------------for displaying the user data-------------------
 let displayUserCard= async function(){
     let mainDiv=document.querySelector(".main");
+   let html='';
     let users=await fetchUser();
-     users.forEach((user)=>{
-//----------------------------appending data--------------------------------------
-        let cardDiv=document.createElement('div');
-  cardDiv.classList.add('card');
-  mainDiv.appendChild(cardDiv);
-  //--------------------------
-        let upperDiv=document.createElement('div');
-  upperDiv.classList.add('upper');
-  cardDiv.appendChild(upperDiv);
-  //--------------------
-  let myImg=document.createElement('img');
-  myImg.classList.add('images');
-  myImg.src='avatar-3637425_960_720.png'
-  upperDiv.appendChild(myImg);
-  // ------------------
-  let lowerDiv=document.createElement('div');
-  lowerDiv.classList.add('lower');
-  cardDiv.appendChild(lowerDiv);
-  //----------------
-  let myHeading=document.createElement('h1');
-  myHeading.id="heading";
-  myHeading.textContent=user.username;
-  lowerDiv.appendChild(myHeading);
-  //-------------------------
-  let myParagraph=document.createElement('p');
-  myParagraph.classList.add('p1');
-  myParagraph.textContent=user.email;
-  lowerDiv.appendChild(myParagraph);
-  //------------------------
-  let submitButton=document.createElement('button');
-  submitButton.classList.add('submit');
-  submitButton.type='submit';
-  lowerDiv.appendChild(submitButton);
-  //-------------------------
-  let anchorTag=document.createElement('a');
-  anchorTag.id="click"
-  anchorTag.href=`user.html`;
-  //-----------------sending the username to the url search path------------------------
-  anchorTag.search=`${user.id}`
-  console.log(anchorTag);
-  anchorTag.textContent="More Information";
-  submitButton.appendChild(anchorTag);
-  //-------------------------
-});
+    users.forEach((user)=>{
+      //making new url
+      let url = new URL(`http://127.0.0.1:5500/user.html?id=${user.id}&name=${user.name}`);
+    //getting value of id and name
+      let params = new URLSearchParams(url.search);
+    html +=`<div class="card">
+    <div class="upper">
+    <img class="images" src="avatar-3637425_960_720.png" alt="" />
+    </div>
+    <div class="lower">
+    <h1>${user.username}</h1>
+    <p class="p1">${user.email}</p>
+    <a class="submit" " href="user.html?${params}">More Information</a>
+    </div>
+    </div> `
+  });
+mainDiv.innerHTML=html;
 }
 displayUserCard();
 
 //----------------------------------------------------------------------------------------------------------
-//---------------------------------display the user full information--------------------
-let showMoreInformation=async function(id){
-  let users= await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  let userId=await users.json();
-  let mainDiv=document.querySelector(".usersData")
-  //------------------user address-------
-  let userAddress=userId.address;
-  let addressGeo=userAddress.geo;
-  // //-------------user company
-  let company=userId.company
-   console.log(userId);
-   let html='';
-//---------------------appending data------------------------------
-html +=`
-<div class="grid-1x3">
-<div class="userdetails">
-<img class="userimage1" src="avatar-3637425_960_720.png">
-<div class="addresslabel">
-<label class="userlab">User Details</label>
-</div>
-  <label class="lab">UserId:-</label>
-  <span class="details">${userId.id}</span>
-  <label class="lab">User Name:-</label>
-  <span class="details">${userId.name}</span>
-  <label class="lab">User Username:-</label>
-  <span class="details">${userId.username}</span>
-  <label class="lab">User Email:-</label>
-  <span class="details">${userId.email}</span>
-  <label class="lab">User Phone No.:-</label>
-  <span class="details">${userId.phone}</span>
-  </div>
-  
-  <div class="second">
-  <div class="Address">
-  <div class="addresslabel">
-  <label>User Address</label>
-  </div>
-  <br>
-  <label class="lab">Street:-</label>
-  <span class="details">${userAddress.street}</span>
-  <label class="lab">Suite:-</label>
-    <span class="details">${userAddress.suite}</span>
-    <label class="lab">City:-</label>
-    <span class="details">${userAddress.city}</span>
-    <label class="lab">ZipCode:-</label>
-    <span class="details">${userAddress.zipcode}</span>
-    <label class="lab">Latitude:-</label>
-    <span class="details">lat:${addressGeo.lat}</span>
-    <label class="lab">Longitude:-</label>
-    <span class="details">lng:${addressGeo.lng} </span>
-    </div>
-    <br>
-    <div class="Company">
-    <div class="addresslabel">
-    <label>User Company</label>
-    </div>
-    <br>
-    <label class="lab">Name:-</label>
-  <span class="details">${company.name}</span>
-  <label class="lab">catchPhrase:-</label>
-    <span class="details">${company.catchPhrase}</span>
-    <label class="lab">bs:-</label>
-    <span class="details">${company.bs}</span>
-       </div>
-</div>
-</div>
-<div class="btndiv">
-    <button class="submit2"><a href="index.html">Go back</a></button>
-  </div>`
-mainDiv.innerHTML = html;
-}
-      //---------------------getting the value of url search path------------------
-      var id=window.location.search.slice(1);
-      console.log(id);
-showMoreInformation(id);
